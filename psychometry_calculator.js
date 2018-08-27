251c38fe2d66a3bcb1187a9be928fe1c7504a29e
Dan <danzlotnikov@gmail.com>
2018-08-27 14:11:12 +0300

    width:100% for mobile 6

--- psychometry_calculator.js
+++ psychometry_calculator.js
@@ -1,16 +1,4 @@
-function setCss() {
-    var cssPath = "psychometry_calculator.css";
-    if (navigator.platform != "iPhone")
-        cssPath = "mobile.css";
 
-    var fileref = document.createElement("link");
-
-    fileref.setAttribute("rel", "stylesheet");
-    fileref.setAttribute("type", "text/css");
-    fileref.setAttribute("href", cssPath);
-
-    document.getElementsByTagName("head")[0].appendChild(fileref);
-}
 function getResults() {
     fixAnswers();
     getHebrewScore();
